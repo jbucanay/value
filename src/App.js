113654dc6dcd6f27b5d46c2ea84a8d8bce50b605
Gilbert Baracka <gibbs@Gilberts-MacBook-Air.local>
2019-04-27 17:58:00 -0500

    'restart'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
-import Layout from "./components/layout/Layout";
+
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Layout title="chat app BAby" />
     </div>
   );
 }
